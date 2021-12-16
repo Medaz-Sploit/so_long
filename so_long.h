e0f895aacd92890577279dc07a24ecc1c22ce322
Medaz-Sploit <azoukni_mohamed@protonmail.com>
2021-12-16 16:38:10 +0100

    Done

---
+++ so_long.h
@@ -0,0 +1,23 @@
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+#include <unistd.h>
+#include <fcntl.h>
+
+
+typedef struct s_so_long
+{
+    struct 
+}               t_so_long;
+
+typedef struct s_map
+{
+
+}               t_map;
+
+typedef struct s_player
+{
+
+}              t_player;
+
+typedef struct 
